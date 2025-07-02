0d75697a9cf598057e08bb4a47b5b4fc3469254c
Bunty Prasad Nayak <78164656+buntyprasadnayak@users.noreply.github.com>
2025-07-02 19:07:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 CORS(app)
 
 # === CONFIG ===
-API_KEY = "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"  # 🔁 Replace this with your real OpenRouter key
-MODEL_NAME = "mistralai/mistral-7b-instruct"  # You can change to openai/gpt-3.5-turbo
+# API_KEY = "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" 
+ # 🔁 Replace this with your real OpenRouter key
+
+API_KEY = "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"
+MODEL_NAME = "mistralai/mistral-small-3.2-24b-instruct-2506:free"  # You can change to openai/gpt-3.5-turbo
 
 OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
 HEADERS = {
